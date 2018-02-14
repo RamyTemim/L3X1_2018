0096331da97fe11dbfc1e8d872e8e36866c2ea1a
tabtihocine <tabti.hocine@outlook.fr>
2018-02-14 14:25:33 +0100

    repére face

--- comparateurApiFace/src/main/java/ComparateurApi.java
+++ comparateurApiFace/src/main/java/ComparateurApi.java
@@ -1,7 +1,5 @@
 import com.google.cloud.vision.v1.*;
 import com.google.protobuf.ByteString;
-
-
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,36 +10,19 @@ import java.util.List;
 public class ComparateurApi {
 
 
-   private static  String  filepath="/home/hocine/L3X1_2018/comparateurApiFace/src/main/resources/tabti_hocine1.JPG";
-
-    public static void main(String[] argc) throws Exception{
+    public static void main(String[] argc) throws Exception {
+        String filepath = "/home/hocine/L3X1_2018/comparateurApiFace/src/main/resources/tabti_hocine1.JPG";
         detectFaces(filepath, System.out);
         //parameter_procesing(argc,System.out);
     }// end main
 
-   /* public static void parameter_procesing (String[]argc, PrintStream out) throws Exception{
-         if(argc.length<1){
-             out.println("Error!! --> use:  ");
-             out.println("java -jar name_of_program path_to_picture");
-             return;
-         }
-          // path to picture
-         String command =argc[0];
-         String path = argc[1];
-        if (command.equals("faces")) {
-                detectFaces(path, out);
-            }// end if
-        }// end paramater_processing */
 
-
-    private static void detectFaces(String Path, PrintStream out) throws Exception {
+    private static void detectFaces(String filepath, PrintStream out) throws Exception {
         //List<AnnotateImageRequest> requests = new ArrayList<>();
 
 
         try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
 
-            // The path to the image file to annotate
-            // String fileName = "/home/hocine/IdeaProjects/FaceDetect/src/main/resources/axel_tabti.jpg";
 
             // Reads the image file into memory
 
@@ -51,6 +32,7 @@ public class ComparateurApi {
 
             // Builds the image annotation request
             List<AnnotateImageRequest> requests = new ArrayList<>();
+
             Image img = Image.newBuilder().setContent(imgBytes).build();
             Feature feat = Feature.newBuilder().setType(Feature.Type.FACE_DETECTION).build();
             AnnotateImageRequest request = AnnotateImageRequest.newBuilder()
@@ -70,16 +52,12 @@ public class ComparateurApi {
                 }
 
                 for (FaceAnnotation annotation : res.getFaceAnnotationsList()) {
-                    out.printf(
-                            "anger: %s\njoy: %s\nsurprise: %s\nposition: %s",
-                            annotation.getAngerLikelihood(),
-                            annotation.getJoyLikelihood(),
-                            annotation.getSurpriseLikelihood(),
-                            annotation.getBoundingPoly());
+                    out.println("Landmark :" );
+                    out.printf("",annotation.getBoundingPoly(),annotation.getLandmarksList());
                 }
             }
         }
 
 
     } // end detectFaces
-}
+}//end class
