d1a9c93b1cedd0c8935e87d4a031e6330cbb32c1
yoyozaza55 <yonizana@gmail.com>
2018-03-04 21:16:50 +0100

    Ajout de l'application SpringBoot pour Microsoft

--- API_Microsoft/src/main/java/yoniz/l3x1/apiFace/IdAPI.java
+++ API_Microsoft/src/main/java/yoniz/l3x1/apiFace/IdAPI.java
@@ -3,10 +3,10 @@ package yoniz.l3x1.apiFace;
 public interface IdAPI {
 
     //Clé pour accéder à l'Api Face
-    //String subscriptionKey = "54fc76374ac34667b970aad66bff6b6d";
+    String subscriptionKey = "54fc76374ac34667b970aad66bff6b6d";
 
     // 2
-    String subscriptionKey = "081ada40e80e4a5c9be2ceb2f9f06941";
+    //String subscriptionKey = "081ada40e80e4a5c9be2ceb2f9f06941";
 
     //Url pour accéder à la fonction de detection des visages dans l'API face
     String uriBaseDetect = "https://northeurope.api.cognitive.microsoft.com/face/v1.0/detect";
