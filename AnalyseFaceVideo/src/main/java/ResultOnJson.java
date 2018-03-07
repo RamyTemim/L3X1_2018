9c0aa589b8c5001f7ae2049c93727c3745568ad8
tabti hocine <tabti.hocine@outlok.fr>
2018-03-07 15:21:24 +0100

    réorganication des classe et

---
+++ AnalyseFaceVideo/src/main/java/ResultOnJson.java
@@ -0,0 +1,61 @@
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultOnJson {
+
+
+    public static JSONObject getJsonObjectAmazon() throws Exception {
+
+        List<String> listpathTophoto = JsonUtil.ReadFile(VideoDetect.pathPhoto);
+        List<String> listpathToVideo = JsonUtil.ReadFile(VideoDetect.pathVideo);
+
+        S3operation.CreatBucket(Var.bucketPhoto);
+        for (String aListpathTophoto : listpathTophoto)
+        {
+            S3operation.UploadFileToBucket(Var.bucketPhoto, aListpathTophoto);
+        }
+
+        S3operation.CreatBucket(Var.bucketVideo);
+        for (String aListpathToVideo : listpathToVideo)
+        {
+            S3operation.UploadFileToBucket(Var.bucketVideo, aListpathToVideo);
+        }
+
+        VideoDetect.credentials =CreatCollectionFaces.connexionIdexFace();
+        CreatCollectionFaces.DeleteCollection(Var.collectionId,VideoDetect.credentials);
+        CreatCollectionFaces.CreatCollectionFace(VideoDetect.credentials, Var.collectionId);
+
+        List<String> listnameOfImage = S3operation.ListFilesInBucket(Var.bucketPhoto);
+        for (int i=0; i<listnameOfImage.size();i++)
+        {
+            CreatCollectionFaces.addFace(VideoDetect.credentials, Var.bucketPhoto, listnameOfImage.get(i) ,Var.collectionId);
+        }
+
+        List<String> listnameOfVideos = S3operation.ListFilesInBucket(Var.bucketVideo);
+        List<List<String>> listImageInVideos = new ArrayList<List<String>>();
+
+        for (int i =0 ; i<listnameOfVideos.size();i++)
+        {
+            listImageInVideos.add(DetectFaceInVideo.DetectFacesInVideos(Var.bucketVideo, listnameOfVideos.get(i), Var.rek, Var.channel, Var.collectionId, Var.queueUrl, Var.sqs));
+        }
+
+        for (int i=0; i<listnameOfImage.size(); i++)
+        {
+            for ( int j=0; j<listImageInVideos.size(); j++ )
+            {
+                if (listImageInVideos.get(j).contains(listnameOfImage.get(i)))
+                {
+                    Var.valeuOfValeuAmazon.append(listnameOfImage.get(i) , listnameOfVideos.get(j) );
+                }
+            }
+        }
+        Var.valueAmazon.put(Var.valeuOfValeuAmazon);
+        Var.jsonObjectAmazon.append("Amazon", Var.valueAmazon);
+       //System.out.println(Var.jsonObjectAmazon);
+        return Var.jsonObjectAmazon;
+    }// END getJsonObjectAmazon
+
+}
--- AnalyseFaceVideo/src/main/java/VideoDetect.java
+++ AnalyseFaceVideo/src/main/java/VideoDetect.java
@@ -1,108 +1,18 @@
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.rekognition.AmazonRekognition;
-import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.NotificationChannel;
-import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class VideoDetect {
 
+    public static   AWSCredentials credentials;
+    public static String  pathPhoto = "src/main/resources/listePhoto.txt";
+    public static String  pathVideo = "src/main/resources/listeVideo.txt";
+
     public static void main(String[] args)  throws Exception
     {
+        JSONObject resultOfAnalyse;
+        resultOfAnalyse= ResultOnJson.getJsonObjectAmazon();
+        System.out.println(resultOfAnalyse);
 
-        String  collectionId = "yoni";
-        String  bucketPhoto = "lxphoto";
-        String  bucketVideo = "lxvideo";
-        String  queueUrl =  "https://sqs.us-east-1.amazonaws.com/027932523227/FileDattenteVideo";
-        NotificationChannel channel = new NotificationChannel().withSNSTopicArn("arn:aws:sns:us-east-1:027932523227:analyse-video")
-                .withRoleArn("arn:aws:iam::027932523227:role/Rekognition");
-        AWSCredentials credentials;
-        String  pathPhoto = "src/main/resources/listePhoto.txt";
-        String  pathVideo = "src/main/resources/listeVideo.txt";
-
-
-        List<String> listpathTophoto = JsonUtil.ReadFile(pathPhoto);
-        List<String> listpathToVideo = JsonUtil.ReadFile(pathVideo);
-
-        S3operation.CreatBucket(bucketPhoto);
-        for (String aListpathTophoto : listpathTophoto)
-        {
-            S3operation.UploadFileToBucket(bucketPhoto, aListpathTophoto);
-        }
-
-        S3operation.CreatBucket(bucketVideo);
-        for (String aListpathToVideo : listpathToVideo)
-        {
-            S3operation.UploadFileToBucket(bucketVideo, aListpathToVideo);
-        }
-
-
-        credentials =CreatCollectionFaces.connexionIdexFace();
-        CreatCollectionFaces.DeleteCollection(collectionId,credentials);
-        CreatCollectionFaces.CreatCollectionFace(credentials, collectionId);
-
-        List<String> listnameOfImage = S3operation.ListFilesInBucket(bucketPhoto);
-        for (int i=0; i<listnameOfImage.size();i++)
-        {
-            CreatCollectionFaces.addFace(credentials, bucketPhoto, listnameOfImage.get(i) ,collectionId);
-        }
-
-
-
-        AmazonSNS  sns = AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
-        AmazonSQS  sqs = AmazonSQSClientBuilder.standard().withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
-        AmazonRekognition  rek = AmazonRekognitionClientBuilder.standard().withCredentials( new ProfileCredentialsProvider())
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://rekognition.us-east-1.amazonaws.com", "us-east-1")).build();
-
-
-        List<String> listnameOfVideos = S3operation.ListFilesInBucket(bucketVideo);
-        List<List<String>> listImageInVideos = new ArrayList<List<String>>();
-
-        for (int i =0 ; i<listnameOfVideos.size();i++)
-        {
-           listImageInVideos.add(DetectFaceInVideo.DetectFacesInVideos(bucketVideo, listnameOfVideos.get(i), rek, channel, collectionId, queueUrl, sqs));
-        }
-
-
-
-        JSONObject jsonObjectAmazon = new JSONObject();
-        JSONArray valueAmazon = new JSONArray();
-        JSONObject valeuOfValeuAmazon =  new JSONObject();
-
-
-        for (int i=0; i<listnameOfImage.size(); i++)
-       {
-           for ( int j=0; j<listImageInVideos.size(); j++ )
-           {
-               if (listImageInVideos.get(j).contains(listnameOfImage.get(i)))
-               {
-
-                  //System.out.println(listnameOfImage.get(i) + " est dans la video " + listnameOfVideos.get(j));
-                   valeuOfValeuAmazon.append(listnameOfImage.get(i) , listnameOfVideos.get(j) );
-               }
-
-           }
-
-
-       }
-        valueAmazon.put(valeuOfValeuAmazon);
-        jsonObjectAmazon.append("Amazon",valueAmazon);
-
-       System.out.println(jsonObjectAmazon);
-
-}// END MAIN
-
+    }// END MAIN
 }// END CLASS
